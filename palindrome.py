529bd62e0cfcdd6155a8a4ec7a08b39e913834cd
shivankar <shivankarpilligundla@gmail.com>
2022-11-25 02:43:35 +0530

    added palindrome

---
+++ palindrome.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+
+def palindrome(string):
+    return string == string[::-1]
+
--- tester.py
+++ tester.py
@@ -2,6 +2,7 @@
 
 import unittest
 from substring import check
+from palindrome import palindrome
 
 class Calculator(unittest.TestCase):
 
@@ -16,6 +17,16 @@ class Calculator(unittest.TestCase):
         sub_str = "cda"
         result = check(str, sub_str)
         self.assertEqual(result, -1)
+    
+    def pal(self):
+        str = "aba"
+        result = palindrome(str)
+        self.assertEqual(result, True)
+    def not_pal(self):
+        str = "abc"
+        result = palindrome(str)
+        self.assertEqual(result, False)
+
     
 
 if __name__ == '__main__':
